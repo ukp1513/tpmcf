c02558b862a9fff9c9a1a064f94b1a3474d2a54e
Krishna <ukp1513@gmail.com>
2024-08-25 20:50:22 +0200

    3d

--- tpmcf/threeD_mcf.py
+++ tpmcf/threeD_mcf.py
@@ -17,69 +17,78 @@ from concurrent.futures import ProcessPoolExecutor, as_completed
 from . import jkgen
 
 
-def omegaTheta(ra_real, dec_real, ra_rand, dec_rand, th_min=0.001, th_max=50.0, bin_size=0.5, ra_units='deg', dec_units='deg', sep_units='degrees'):
+
+def comovingDistanceH0(redshift, cosmology):
+	comDist = cosmology.comoving_distance(redshift)*cosmology.H0/100.
+	return comDist
+	
+def xiS(ra_real, dec_real, dist_real, ra_rand, dec_rand, dist_rand, s_min=5.0, s_max=5000.0, bin_size=10.0, bin_type='Linear', ra_units='deg', dec_units='deg'):
 
 	# Create catalog for the data
-	cat_real = treecorr.Catalog(ra=ra_real, dec=dec_real, ra_units=ra_units, dec_units=dec_units)
-	dd = treecorr.NNCorrelation(min_sep=th_min, max_sep=th_max, bin_size=bin_size, sep_units = sep_units)
+	cat_real = treecorr.Catalog(ra=ra_real, dec=dec_real, r=dist_real, ra_units=ra_units, dec_units=dec_units)
+	dd = treecorr.NNCorrelation(bin_type=bin_type, min_sep=s_min, max_sep=s_max, bin_size=bin_size)
 	dd.process(cat_real)
 
 	# Create catalog for the randoms
-	cat_rand = treecorr.Catalog(ra=ra_rand, dec=dec_rand, ra_units=ra_units, dec_units=dec_units)
-	rr = treecorr.NNCorrelation(min_sep=th_min, max_sep=th_max, bin_size=bin_size, sep_units = sep_units)
+	cat_rand = treecorr.Catalog(ra=ra_rand, dec=dec_rand, r=dist_rand, ra_units=ra_units, dec_units=dec_units)
+	rr = treecorr.NNCorrelation(bin_type=bin_type, min_sep=s_min, max_sep=s_max, bin_size=bin_size)
 	rr.process(cat_rand)
 
 	# Create their cross catalog
-	dr = treecorr.NNCorrelation(min_sep=th_min, max_sep=th_max, bin_size=bin_size, sep_units = sep_units)
+	dr = treecorr.NNCorrelation(bin_type=bin_type, min_sep=s_min, max_sep=s_max, bin_size=bin_size)
 	dr.process(cat_real, cat_rand)
 	
 	# Calculate 2pt correlation function of the total sample
-	omega, varomega = dd.calculateXi(rr=rr, dr=dr)
-	th = np.exp(dd.meanlogr)
+	xi, varxi = dd.calculateXi(rr=rr, dr=dr)
+	s = np.exp(dd.meanlogr)
 
-	return th, omega
+	return s, xi
 	
-def weightedOmegaTheta(ra_real, dec_real, weight_real, ra_rand, dec_rand, th_min=0.001, th_max=50.0, bin_size=0.5, ra_units='deg', dec_units='deg', sep_units='degrees'):
+def weightedXiS(ra_real, dec_real, dist_real, weight_real, ra_rand, dec_rand, dist_rand, s_min=5.0, s_max=5000.0, bin_size=10.0, bin_type='Linear', ra_units='deg', dec_units='deg'):
 
 	# Create catalog for the data
-	cat_real = treecorr.Catalog(ra=ra_real, dec=dec_real, w=weight_real, ra_units=ra_units, dec_units=dec_units)
-	ww = treecorr.NNCorrelation(min_sep=th_min, max_sep=th_max, bin_size=bin_size, sep_units = sep_units)
+	cat_real = treecorr.Catalog(ra=ra_real, dec=dec_real, w=weighted_real, r=dist_real, ra_units=ra_units, dec_units=dec_units)
+	ww = treecorr.NNCorrelation(bin_type=bin_type, min_sep=s_min, max_sep=s_max, bin_size=bin_size)
 	ww.process(cat_real)
 
 	# Create catalog for the randoms
-	cat_rand = treecorr.Catalog(ra=ra_rand, dec=dec_rand, ra_units=ra_units, dec_units=dec_units)
-	rr = treecorr.NNCorrelation(min_sep=th_min, max_sep=th_max, bin_size=bin_size, sep_units = sep_units)
+	cat_rand = treecorr.Catalog(ra=ra_rand, dec=dec_rand, r=dist_rand, ra_units=ra_units, dec_units=dec_units)
+	rr = treecorr.NNCorrelation(bin_type=bin_type, min_sep=s_min, max_sep=s_max, bin_size=bin_size)
 	rr.process(cat_rand)
 
 	# Create their cross catalog
-	wr = treecorr.NNCorrelation(min_sep=th_min, max_sep=th_max, bin_size=bin_size, sep_units = sep_units)
+	wr = treecorr.NNCorrelation(bin_type=bin_type, min_sep=s_min, max_sep=s_max, bin_size=bin_size)
 	wr.process(cat_real, cat_rand)
 	
 	# Calculate 2pt correlation function of the total sample
-	weighted_omega, var_weightedomega = ww.calculateXi(rr=rr, dr=wr)
-	th = np.exp(ww.meanlogr)
+	weighted_xi, var_weightedxi = dd.calculateXi(rr=rr, dr=dr)
+	s = np.exp(dd.meanlogr)
 
-	return th, weighted_omega
+	return s, weighted_xi
 	
-
-def mcfTheta(th, omega_th, weighted_omega_th):
-	M_th = (1 + weighted_omega_th)/(1 + omega_th)
-	return M_th
+def mcfS(s, xi_s, weighted_xi_s):
+	M_s = (1 + weighted_xi_s)/(1 + xi_s)
+	return M_s
 	
-def computeCF(real_tab, real_properties, rand_tab, realracol='RA',realdeccol='DEC',randracol='RA', randdeccol='Dec'):
+def computeCF(real_tab, real_properties, rand_tab, realracol, realdeccol, realzcol, randracol, randdeccol, randzcol, cosmology):
 
 	ra_real = real_tab[realracol]
 	dec_real = real_tab[realdeccol]
+	z_real = real_tab[realzcol]
 
 	ra_rand = rand_tab[randracol]
 	dec_rand = rand_tab[randdeccol]
+	z_rand = rand_tab[randzcol]
+	
+	dist_real = comoving_distance_H0(z_real, cosmology)
+	dist_rand = comoving_distance_H0(z_rand, cosmology)
 
-	th, omega = omegaTheta(ra_real, dec_real, ra_rand, dec_rand)
+	s, xi = xiS(ra_real, dec_real, dist_real, ra_rand, dec_rand, dist_rand)
 	
-	th_omega_mcfs = np.empty((len(th), 0))
+	s_xi_mcfs = np.empty((len(s), 0))
 	
-	th_omega_mcfs = np.hstack((th_omega_mcfs, th.reshape(len(th), 1)))
-	th_omega_mcfs = np.hstack((th_omega_mcfs, omega.reshape(len(th), 1)))
+	s_xi_mcfs = np.hstack((s_xi_mcfs, s.reshape(len(s), 1)))
+	s_xi_mcfs = np.hstack((s_xi_mcfs, xi.reshape(len(s), 1)))
 	
 	for prop_i in real_properties:
 	
@@ -87,16 +96,19 @@ def computeCF(real_tab, real_properties, rand_tab, realracol='RA',realdeccol='DE
 		
 		prop_now_ranked = rankdata(prop_now)
 	
-		th, weighted_omega_ranked = weightedOmegaTheta(ra_real, dec_real, prop_now_ranked, ra_rand, dec_rand)
+		s, weighted_xi_ranked = weightedXiS(ra_real, dec_real, dist_real, prop_now_ranked, ra_rand, dec_rand, dist_rand)
 
-		M_theta = np.array(mcfTheta(th, omega, weighted_omega_ranked)).reshape(len(th), 1)
+		M_s = np.array(mcfS(s, xi, weighted_xi_ranked)).reshape(len(s), 1)
 				
-		th_omega_mcfs = np.hstack((th_omega_mcfs, M_theta))
+		s_xi_mcfs = np.hstack((s_xi_mcfs, M_s))
 		
-	return th_omega_mcfs
+	return s_xi_mcfs
 	
 	
-def runComputation(real_tab, real_properties, rand_tab, njacks_ra, njacks_dec, working_dir=os.getcwd(), realracol='RA',realdeccol='DEC',randracol='RA', randdeccol='Dec'):
+def runComputation(real_tab, real_properties, rand_tab, njacks_ra, njacks_dec, working_dir=os.getcwd(), realracol='RA',realdeccol='DEC', realzcol='redshift', randracol='RA', randdeccol='Dec', randzcol='redshift', cosmology_H0_Om0=[70.0, 0.3]):
+
+	H0, Om0=cosmology_H0_Om0
+	cosmology = FlatLambdaCDM(H0=H0, Om0=Om0)
 
 	os.chdir(working_dir)
 	os.mkdir('biproducts')
@@ -117,7 +129,7 @@ def runComputation(real_tab, real_properties, rand_tab, njacks_ra, njacks_dec, w
 			real_tab_i, rand_tab_i = jkgen.giveJkSample(jk_i, real_tab, rand_tab, njacks_ra=njacks_ra, njacks_dec=njacks_dec, realracol=realracol, realdeccol=realdeccol, randracol=randracol, randdeccol=randdeccol)
 			result_file = 'results/jackknifes/CFJackknife_jk%d.txt' %jk_i
 			
-		result_i = computeCF(real_tab_i, real_properties, rand_tab_i, realracol, realdeccol, randracol, randdeccol)
+		result_i = computeCF(real_tab_i, real_properties, rand_tab_i, realracol, realdeccol, realzcol, randracol, randdeccol, randzcol, cosmology=cosmology)
 		
 		np.savetxt(result_file, result_i, delimiter="\t",fmt='%f')
 	
