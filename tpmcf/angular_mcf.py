bdae71a17d99198337520d9b886d48272e38db48
Krishna <ukp1513@gmail.com>
2024-10-19 11:24:21 +0200

    omp

--- tpmcf/angular_mcf.py
+++ tpmcf/angular_mcf.py
@@ -11,26 +11,24 @@ from astropy.io import fits
 from astropy import units as u
 from astropy.coordinates import SkyCoord
 import os
-
-from concurrent.futures import ProcessPoolExecutor, as_completed
+from concurrent.futures import ProcessPoolExecutor
 
 from . import jkgen
 
-
-def omegaTheta(ra_real, dec_real, ra_rand, dec_rand, th_min=0.001, th_max=50.0, bin_size=0.5, ra_units='deg', dec_units='deg', sep_units='degrees'):
+def omegaTheta(ra_real, dec_real, ra_rand, dec_rand, th_min=0.001, th_max=50.0, nbins=8, ra_units='deg', dec_units='deg', sep_units='degrees'):
 
 	# Create catalog for the data
 	cat_real = treecorr.Catalog(ra=ra_real, dec=dec_real, ra_units=ra_units, dec_units=dec_units)
-	dd = treecorr.NNCorrelation(min_sep=th_min, max_sep=th_max, bin_size=bin_size, sep_units = sep_units)
+	dd = treecorr.NNCorrelation(min_sep=th_min, max_sep=th_max, nbins=nbins, sep_units = sep_units)
 	dd.process(cat_real)
 
 	# Create catalog for the randoms
 	cat_rand = treecorr.Catalog(ra=ra_rand, dec=dec_rand, ra_units=ra_units, dec_units=dec_units)
-	rr = treecorr.NNCorrelation(min_sep=th_min, max_sep=th_max, bin_size=bin_size, sep_units = sep_units)
+	rr = treecorr.NNCorrelation(min_sep=th_min, max_sep=th_max, nbins=nbins, sep_units = sep_units)
 	rr.process(cat_rand)
 
 	# Create their cross catalog
-	dr = treecorr.NNCorrelation(min_sep=th_min, max_sep=th_max, bin_size=bin_size, sep_units = sep_units)
+	dr = treecorr.NNCorrelation(min_sep=th_min, max_sep=th_max, nbins=nbins, sep_units = sep_units)
 	dr.process(cat_real, cat_rand)
 	
 	# Calculate 2pt correlation function of the total sample
@@ -39,20 +37,20 @@ def omegaTheta(ra_real, dec_real, ra_rand, dec_rand, th_min=0.001, th_max=50.0, 
 
 	return th, omega
 	
-def weightedOmegaTheta(ra_real, dec_real, weight_real, ra_rand, dec_rand, th_min=0.001, th_max=50.0, bin_size=0.5, ra_units='deg', dec_units='deg', sep_units='degrees'):
+def weightedOmegaTheta(ra_real, dec_real, weight_real, ra_rand, dec_rand, th_min=0.001, th_max=50.0, nbins=8, ra_units='deg', dec_units='deg', sep_units='degrees'):
 
 	# Create catalog for the data
 	cat_real = treecorr.Catalog(ra=ra_real, dec=dec_real, w=weight_real, ra_units=ra_units, dec_units=dec_units)
-	ww = treecorr.NNCorrelation(min_sep=th_min, max_sep=th_max, bin_size=bin_size, sep_units = sep_units)
+	ww = treecorr.NNCorrelation(min_sep=th_min, max_sep=th_max, nbins=nbins, sep_units = sep_units)
 	ww.process(cat_real)
 
 	# Create catalog for the randoms
 	cat_rand = treecorr.Catalog(ra=ra_rand, dec=dec_rand, ra_units=ra_units, dec_units=dec_units)
-	rr = treecorr.NNCorrelation(min_sep=th_min, max_sep=th_max, bin_size=bin_size, sep_units = sep_units)
+	rr = treecorr.NNCorrelation(min_sep=th_min, max_sep=th_max, nbins=nbins, sep_units = sep_units)
 	rr.process(cat_rand)
 
 	# Create their cross catalog
-	wr = treecorr.NNCorrelation(min_sep=th_min, max_sep=th_max, bin_size=bin_size, sep_units = sep_units)
+	wr = treecorr.NNCorrelation(min_sep=th_min, max_sep=th_max, nbins=nbins, sep_units = sep_units)
 	wr.process(cat_real, cat_rand)
 	
 	# Calculate 2pt correlation function of the total sample
@@ -60,13 +58,12 @@ def weightedOmegaTheta(ra_real, dec_real, weight_real, ra_rand, dec_rand, th_min
 	th = np.exp(ww.meanlogr)
 
 	return th, weighted_omega
-	
 
 def mcfTheta(th, omega_th, weighted_omega_th):
 	M_th = (1 + weighted_omega_th)/(1 + omega_th)
 	return M_th
 	
-def computeCF(real_tab, real_properties, rand_tab, thmin, thmax, th_bin_size, realracol='RA',realdeccol='DEC',randracol='RA', randdeccol='Dec'):
+def computeCF(real_tab, real_properties, rand_tab, thmin, thmax, th_nbins, realracol='RA',realdeccol='DEC',randracol='RA', randdeccol='Dec'):
 
 	ra_real = real_tab[realracol]
 	dec_real = real_tab[realdeccol]
@@ -74,7 +71,7 @@ def computeCF(real_tab, real_properties, rand_tab, thmin, thmax, th_bin_size, re
 	ra_rand = rand_tab[randracol]
 	dec_rand = rand_tab[randdeccol]
 
-	th, omega = omegaTheta(ra_real, dec_real, ra_rand, dec_rand, th_min=thmin, th_max=thmax, bin_size=th_bin_size)
+	th, omega = omegaTheta(ra_real, dec_real, ra_rand, dec_rand, th_min=thmin, th_max=thmax, nbins=th_nbins)
 	
 	th_omega_mcfs = np.empty((len(th), 0))
 	
@@ -87,7 +84,7 @@ def computeCF(real_tab, real_properties, rand_tab, thmin, thmax, th_bin_size, re
 		
 		prop_now_ranked = rankdata(prop_now)
 	
-		th, weighted_omega_ranked = weightedOmegaTheta(ra_real, dec_real, prop_now_ranked, ra_rand, dec_rand, th_min=thmin, th_max=thmax, bin_size=th_bin_size)
+		th, weighted_omega_ranked = weightedOmegaTheta(ra_real, dec_real, prop_now_ranked, ra_rand, dec_rand, th_min=thmin, th_max=thmax, nbins=th_nbins)
 
 		M_theta = np.array(mcfTheta(th, omega, weighted_omega_ranked)).reshape(len(th), 1)
 				
@@ -96,7 +93,10 @@ def computeCF(real_tab, real_properties, rand_tab, thmin, thmax, th_bin_size, re
 	return th_omega_mcfs
 	
 	
-def runComputationAngular(real_tab, real_properties, rand_tab, thmin, thmax, th_bin_size, njacks_ra, njacks_dec, working_dir=os.getcwd(), realracol='RA',realdeccol='DEC',randracol='RA', randdeccol='Dec'):
+
+	
+	
+def runComputationAngular(real_tab, real_properties, rand_tab, thmin, thmax, th_nbins, njacks_ra, njacks_dec, working_dir=os.getcwd(), realracol='RA',realdeccol='DEC',randracol='RA', randdeccol='Dec', omp=False):
 
 	os.chdir(working_dir)
 	os.mkdir('biproducts')
@@ -109,7 +109,8 @@ def runComputationAngular(real_tab, real_properties, rand_tab, thmin, thmax, th_
 	
 	n_jacks = njacks_ra * njacks_dec
 	
-	for jk_i in range(n_jacks+1):
+	def process_jackknife(jk_i):
+		
 		if(jk_i == 0):
 			real_tab_i, rand_tab_i = real_tab, rand_tab 
 			result_file = 'results/CFReal.txt'
@@ -119,9 +120,20 @@ def runComputationAngular(real_tab, real_properties, rand_tab, thmin, thmax, th_
 			result_file = 'results/jackknifes/CFJackknife_jk%d.txt' %jk_i
 			print("Working on the jackknife sample %d" %jk_i)
 			
-		result_i = computeCF(real_tab_i, real_properties, rand_tab_i, realracol, realdeccol, randracol, randdeccol)
+		result_i = computeCF(real_tab_i, real_properties, rand_tab_i, thmin, thmax, th_nbins, realracol, realdeccol, randracol, randdeccol)
 		
 		np.savetxt(result_file, result_i, delimiter="\t",fmt='%f')
+		
+		return 0
+	
+	if(omp): #TODO: not working...
+		print("Parellel programming with %d workers..." %(os.cpu_count()))
+		with ProcessPoolExecutor() as executor:
+        		executor.map(process_jackknife, range(n_jacks + 1))
+	
+	else:
+		for jk_i in range(n_jacks+1):
+			process_jackknife(jk_i)
 	
 	return 0
 	
